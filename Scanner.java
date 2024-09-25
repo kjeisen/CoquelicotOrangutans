dad32e396b1f5148ff4de5138349640e2774c77f
asarver <andsarve15@gmail.com>
2024-09-25 18:35:50 -0400

    Shortened make_map

--- Scanner.java
+++ Scanner.java
@@ -9,7 +9,7 @@ class Scanner {
         // get int value of char
         make_map();
         make_array();
-        System.out.println( array.get(current_state.index).get(characterToIndex.get('f')));
+        System.out.println(array.get(current_state.index).get(characterToIndex.get('f')));
         current_state = array.get(current_state.index).get(characterToIndex.get('a'));
         System.out.println(current_state);
     }
@@ -42,88 +42,10 @@ class Scanner {
 
     // Making the map from characters to indexs
     public static void make_map() {
-        characterToIndex.put('a',0);
-        characterToIndex.put('b',1);
-        characterToIndex.put('c',2);
-        characterToIndex.put('d',3);
-        characterToIndex.put('e',4);
-        characterToIndex.put('f',5);
-        characterToIndex.put('g',6);
-        characterToIndex.put('h',7);
-        characterToIndex.put('i',8);
-        characterToIndex.put('j',9);
-        characterToIndex.put('k',10);
-        characterToIndex.put('l',11);
-        characterToIndex.put('m',12);
-        characterToIndex.put('n',13);
-        characterToIndex.put('o',14);
-        characterToIndex.put('p',15);
-        characterToIndex.put('q',16);
-        characterToIndex.put('r',17);
-        characterToIndex.put('s',18);
-        characterToIndex.put('t',19);
-        characterToIndex.put('u',20);
-        characterToIndex.put('v',21);
-        characterToIndex.put('w',22);
-        characterToIndex.put('x',23);
-        characterToIndex.put('y',24);
-        characterToIndex.put('z',25);
-        characterToIndex.put('A',26);
-        characterToIndex.put('B',27);
-        characterToIndex.put('C',28);
-        characterToIndex.put('D',29);
-        characterToIndex.put('E',30);
-        characterToIndex.put('F',31);
-        characterToIndex.put('G',32);
-        characterToIndex.put('H',33);
-        characterToIndex.put('I',34);
-        characterToIndex.put('J',35);
-        characterToIndex.put('K',36);
-        characterToIndex.put('L',37);
-        characterToIndex.put('M',38);
-        characterToIndex.put('N',39);
-        characterToIndex.put('O',40);
-        characterToIndex.put('P',41);
-        characterToIndex.put('Q',42);
-        characterToIndex.put('R',43);
-        characterToIndex.put('S',44);
-        characterToIndex.put('T',45);
-        characterToIndex.put('U',46);
-        characterToIndex.put('V',47);
-        characterToIndex.put('W',48);
-        characterToIndex.put('X',49);
-        characterToIndex.put('Y',50);
-        characterToIndex.put('Z',51);
-        characterToIndex.put('0',52);
-        characterToIndex.put('1',53);
-        characterToIndex.put('2',54);
-        characterToIndex.put('3',55);
-        characterToIndex.put('4',56);
-        characterToIndex.put('5',57);
-        characterToIndex.put('6',58);
-        characterToIndex.put('7',59);
-        characterToIndex.put('8',60);
-        characterToIndex.put('9',61);
-        characterToIndex.put('.',62);
-        characterToIndex.put('-',63);
-        characterToIndex.put('+',64);
-        characterToIndex.put('/',65);
-        characterToIndex.put('*',66);
-        characterToIndex.put('(',67);
-        characterToIndex.put(')',68);
-        characterToIndex.put('{',69);
-        characterToIndex.put('}',70);
-        characterToIndex.put('<',71);
-        characterToIndex.put('>',72);
-        characterToIndex.put('=',73);
-        characterToIndex.put('!',74);
-        characterToIndex.put('_',75);
-    // All whitespace characters go to same place
-        characterToIndex.put(' ',76);
-        characterToIndex.put('\t',76);
-        characterToIndex.put('\n',76);
-        characterToIndex.put('\f',76);
-        characterToIndex.put('\r',76);
+        String tokens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-+/*(){}<>=!_ \t\n\f\r";
+        for (int i = 0; i < tokens.length(); i++) {
+            characterToIndex.put(tokens.charAt(i), i);
+        }
     }
     // Enums for all the states with and index associated
     public enum State {
